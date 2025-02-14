4c7d07280e37e25e76041806921adf1c2c88ba7a
a96640 <a96640@alunos.uminho.pt>
2025-02-14 16:37:57 +0000

    tpc1 atualizado

--- TPC1/tpc1.py
+++ TPC1/tpc1.py
@@ -5,17 +5,24 @@ import re
 soma = 0
 enable = True
 
+
+#Linha de input é lida
 for line in sys.stdin:
     # Divide o texto em palavras "on", "off", dígitos e '='
     palavras = re.findall(r'on|off|\d+|=', line.lower())
     for palavra in palavras:
+        #Quando encontra a sequencia off desliga o comportamento de somar as sequências de digitos
         if palavra == 'off':
             enable = False
+        #E liga o comportamento quando encontra a sequencia on
         elif palavra == 'on':
             enable = True
+        #Quando encontra o carater '='  imprime o resultado da soma até então e reinicia a soma a 0
         elif palavra == '=':
             print(soma)
-            soma = 0
+        #Assim que a sequencia de digitos é encontrada é somada a sequência de digitos ao resultado da soma até então
         elif enable and palavra.isdigit():
             soma += int(palavra)
 
+#No final imprime o resultado da soma
+print(soma)
